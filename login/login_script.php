9ff7a6c4a11300140d1e21a7c2437f7909f2def4
Julian Häring <Julian.Haering@plan-b-gmbh.com>
2020-03-19 16:12:26 +0100

    asd

--- login/login_script.php
+++ login/login_script.php
@@ -16,7 +16,7 @@ if(isset($_POST['submit'])){
     if ($user !== false && password_verify($password, $user['passwort'])) {
         $_SESSION['user'] = $user['benutzername'];
         $_SESSION['isloggedin'] = true;
-        header('Location: https://mathe-abi-vorbereitung.de/');
+        
         
     } else {
         $error = true;
@@ -25,9 +25,48 @@ if(isset($_POST['submit'])){
         header('Location: https://mathe-abi-vorbereitung.de/login/');
     }
 
+    //generieren des Session Tokens
+    if(!$error){
+        //token
+        $token = getToken(10);
+        
+        //SQL
+        $sql = "SELECT * AS anzahl FROM user_token WHERE benutzername = :nutzer";
+        $statement = $pdo->prepare($sql);
+        $statement->execute(array('nutzer' => $benutzername));
+        $token_result = $statement->fetch();
+
+        if($token_result !== false){
+            //update
+            $sql_token = "UPDATE user_token SET token = :tok WHERE benutzername = :nutzer";
+            $statement = $pdo->prepare($sql_token);
+            $statement->execute(array('tok' => $token, 'nutzer' => $benutzername));
+
+        }else{
+            //Insert
+            $sql_token = "INSERT INTO user_token (user_id, benutzername, token) VALUES (:id, :nutzer, :tok)";
+            $statement = $pdo->prepare($sql_token);
+            $statement->execute(array('tok' => $token, 'nutzer' => $benutzername, 'id' => $user['id']));
+        }
+
+    }
+
     
 
-
+// Generate token
+function getToken($length){
+    $token = "";
+    $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $codeAlphabet.= "abcdefghijklmnopqrstuvwxyz";
+    $codeAlphabet.= "0123456789";
+    $max = strlen($codeAlphabet); // edited
+   
+    for ($i=0; $i < $length; $i++) {
+     $token .= $codeAlphabet[random_int(0, $max-1)];
+    }
+   
+    return $token;
+   }
 }
 
 ?>
