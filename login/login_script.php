2913dcc9b829ce254cfa4023e4de111da7d4734b
Julian Häring <Julian.Haering@plan-b-gmbh.com>
2020-03-19 15:54:53 +0100

    asdfg

--- login/login_script.php
+++ login/login_script.php
@@ -5,9 +5,7 @@ $pdo = new PDO('mysql:host=188.68.47.203;dbname=k93814_matheAbi', 'k93814_matheA
 
 if(isset($_POST['submit'])){
     $benutzername = $_POST['benutzer'];
-    echo $benutzername . "<br>";
     $password = $_POST['passwort'];
-    echo $password . "<br>";
 
     //Überprüfung mit Datenbank
     $sql = "SELECT * FROM benutzer WHERE benutzername = :nutzer";
@@ -18,13 +16,13 @@ if(isset($_POST['submit'])){
     if ($user !== false && password_verify($password, $user['passwort'])) {
         $_SESSION['user'] = $user['benutzername'];
         $_SESSION['isloggedin'] = true;
-        header('Location https://mathe-abi-vorbereitung.de/');
+        header('Location: https://mathe-abi-vorbereitung.de/');
         
     } else {
         $error = true;
         $_SESSION['notification'] = "Es wurde kein Benutzer zu den eingegebenen Daten gefunden.";
         echo "falsche Eingaben";
-        //header('Location https://mathe-abi-vorbereitung.de/login/');
+        header('Location: https://mathe-abi-vorbereitung.de/login/');
     }
 
     
