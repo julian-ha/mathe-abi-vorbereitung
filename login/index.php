90371c6c9d55ecf665632d0ce65a0f7e556f5297
Julian Häring <Julian.Haering@plan-b-gmbh.com>
2020-03-19 15:50:24 +0100

    sd

--- login/index.php
+++ login/index.php
@@ -12,9 +12,9 @@ session_start();
         <input type="text" name="benutzer" required><br>
         <input type="password" name="passwort" required><br>
         
-        <?php if(isset($_SESSION['fehlermeldung'])){
-            echo "<p>". $_SESSION['fehlermeldung']. "</p>";
-            unset($_SESSION['fehlermeldung']);
+        <?php if(isset($_SESSION['notification'])){
+            echo "<p>". $_SESSION['notification']. "</p>";
+            unset($_SESSION['notification']);
         } ?>
         <input type="submit" name="submit" value="Abschicken">
         </form>
