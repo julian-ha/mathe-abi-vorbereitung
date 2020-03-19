a2f7e2b48d2b88bcd376aa6e7aa48676f5b61c10
Julian Häring <Julian.Haering@plan-b-gmbh.com>
2020-03-19 15:25:27 +0100

    asd

---
+++ login/login_script.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+$error = false;
+$pdo = new PDO('mysql:host=188.68.47.203;dbname=k93814_matheAbi', 'k93814_matheAbi', 'Sxt0m25?');
+
+if(isset($_POST['submit'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    //Überprüfung mit Datenbank
+    $sql = "SELECT * FROM benutzer WHERE mail = :email AND passwort = :passwort";
+    $statement = $pdo->prepare($sql);
+    $statement->execute(array('email' => $email, 'passwort' => $password));
+    $user = statement->fetch();
+    echo $user['vorname'];
+
+
+}
+
+?>
