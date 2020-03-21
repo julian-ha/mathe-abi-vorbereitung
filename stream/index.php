0c6af3e92bbe26ed32a9399a68531e830fe75d98
envalde <kohoutdanie@gmail.com>
2020-03-21 17:12:20 +0100

    go

--- stream/index.php
+++ stream/index.php
@@ -4,17 +4,16 @@ include('../settings/login_control.php');
 ?>
 <!-- man muss immer den Token in die Variable laden-->
 <script>
-const token = "<?php echo $_SESSION['token'] ?>";
-console.log(token);
+  const token = "<?php echo $_SESSION['token'] ?>";
+  console.log(token);
 </script>
-
 
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Mathe-Abi-Vorbereitung</title>
+    <title>Mathe-Abi-Vorbereitung</title>
     <link
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"
@@ -27,7 +26,55 @@ console.log(token);
     <link rel="stylesheet" href="../includes/style.css" />
     <script src="../js/client.js"></script>
   </head>
+
   <body>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://mathe-abi-vorbereitung.de">
+          <p><strong>Mathe-Abi-Vorbereitung</strong></p>
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a
+            href="https://mathe-abi-vorbereitung.de/#tutor-info"
+            class="navbar-item"
+          >
+            Startseite
+          </a>
+          <!-- <a
+            href="https://mathe-abi-vorbereitung.de/#tutor-info"
+            class="navbar-item"
+          >
+            Backup-Stream
+          </a> -->
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-light" href="/logout/">
+                Logout
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
     <div class="info-section">
       <section class="section">
         <div class="container has-text-centered is-centered">
@@ -48,4 +95,3 @@ console.log(token);
     </div>
   </body>
 </html>
-
