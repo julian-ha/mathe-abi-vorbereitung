650dd0d2233ae63f67d36bf0605bf45b1d284ea6
envalde <kohoutdanie@gmail.com>
2020-03-21 11:47:02 +0100

    go

--- register/index.php
+++ register/index.php
@@ -8,14 +8,21 @@ $pdo = new PDO('mysql:host=188.68.47.203;dbname=k93814_matheAbi', 'k93814_matheA
 
 
 
-<!DOCTYPE html> 
-<html> 
+<!DOCTYPE html>
+<html>
+
 <head>
-  <title>Mathe Abi Vorbereitung</title>    
-</head> 
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Mathe-Abi-Vorbereitung</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css" />
+    <link rel="stylesheet" href="includes/style.css" />
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+</head>
+
 <body>
- 
-<?php
+
+    <?php
 $showFormular = true; //Variable ob das Registrierungsformular anezeigt werden soll
  
 if(isset($_GET['register'])) {
@@ -94,28 +101,71 @@ if(isset($_GET['register'])) {
  
 if($showFormular) {
 ?>
- 
-<form action="?register=1" method="post">
 
-Dein Benutzername: <br>
-<input type="text" maxlength="250" name="benutzername" required> <br>
- 
-Dein Passwort:<br>
-<input type="password" size="40"  maxlength="250" name="passwort" required><br>
-<input type="password" size="40" maxlength="250" name="passwort2" required><br>
-<?php
+    <form action="?register=1" method="post">
+
+        <section class="hero is-light is-fullheight">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="title">
+                        Dein Kauf war erfolgreich
+                    </h1>
+                    <h2 class="subtitle">
+                        Gib nun einen Benutzernamen und ein Passwort ein
+                    </h2>
+                    <div class="columns">
+                        <div class="column register-form">
+                            <div class="field">
+                                <label class="label">Benutzername</label>
+                                <div class="control has-icons-left has-icons-right">
+                                    <input class="input is-primary" type="text" placeholder="Wähle einen Benutzernamen"
+                                        value="" maxlength="250" name="benutzername" required />
+                                    <span class="icon is-small is-left">
+                                        <i class="fas fa-user"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="label">Passwort</label>
+                                <div class="control has-icons-left has-icons-right">
+                                    <input class="input is-primary" type="password" placeholder="Wähle ein Passswort"
+                                        value="" maxlength="250" name="passwort" required />
+                                    <span class="icon is-small is-left">
+                                        <i class="fas fa-user"></i>
+                                    </span>
+                                </div>
+                                <br />
+                                <div class="control has-icons-left has-icons-right">
+                                    <input class="input is-primary" type="password"
+                                        placeholder="Bestätige dein Passswort" value="" maxlength="250" name="passwort"
+                                        required />
+                                    <span class="icon is-small is-left">
+                                        <i class="fas fa-user"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+        <?php
 if(isset($_SESSION['fehler'])){
     echo "<p>" . $_SESSION['fehler'] . "</p>";
     unset($_SESSION['fehler']);
 }
 ?>
- 
-<input type="submit" value="Abschicken">
-</form>
- 
-<?php
+
+        <input class="button is-primary" type="submit" value="Registrieren">
+    </form>
+
+    <?php
 } //Ende von if($showFormular)
 ?>
- 
+
 </body>
+
 </html>
