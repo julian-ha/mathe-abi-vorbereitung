ae3d85040268215d821872fe3a0423b77065fd7a
Julian Häring <julian@julians-mini.speedport_w_921v_1_46_000>
2020-03-22 11:39:21 +0100

    sdf

--- chat/messenger/index.php
+++ chat/messenger/index.php
@@ -33,15 +33,17 @@ foreach($messages as $message){
         <div class="container">
             <div class="columns">
                 <div class="column is-full">
+                    <?php foreach($message as $message) { ?>
                     <article class="message">
                         <div class="message-header">
-                            <p>Hello World</p>
+                            <p>Nachricht von Nutzer: <?php echo $message['benutzername'] ?></p>
                             <button class="delete" aria-label="delete"></button>
                         </div>
                         <div class="message-body">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
                         </div>
                     </article>
+                    <?php } ?>
                 </div>
             </div>
         </div>
