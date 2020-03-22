aacca5b3ee61b438bfaa91e07f050ca61139ed8a
Julian Häring <julian@julians-mini.speedport_w_921v_1_46_000>
2020-03-22 11:36:00 +0100

    sd

--- chat/messenger/index.php
+++ chat/messenger/index.php
@@ -14,3 +14,30 @@ foreach($messages as $message){
     echo $message['benutzername'] . "<br>";
 }
 ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Mathe-Abi-Vorbereitung - STREAM</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css" />
+    <link rel="stylesheet" href="includes/style.css" />
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <link rel="stylesheet" href="../includes/style.css" />
+    
+</head>
+    <body>
+    <article class="message">
+  <div class="message-header">
+    <p>Hello World</p>
+    <button class="delete" aria-label="delete"></button>
+  </div>
+  <div class="message-body">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+  </div>
+</article>
+        
+
+    </body>
+</html>
