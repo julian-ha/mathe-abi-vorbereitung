442ee3f34d125738df411cc036d58c42d343afc2
Julian Häring <julian@julians-mini.speedport_w_921v_1_46_000>
2020-03-22 12:23:26 +0100

    sdf

--- chat/messenger/index.php
+++ chat/messenger/index.php
@@ -44,7 +44,7 @@ $messages = $statement->fetchAll();
                     <article class="message <?php echo $message['farbe'] ?>">
                         <div class="message-header">
                             <p>Nachricht von Nutzer: <strong><?php echo $message['benutzername'] ?></strong></p>
-                            <span style="float:right">Uhrzeit</span>
+                            <span style="float:right">Uhrzeit: <?php $message['zeitstempel'] ?></span>
                             
                         </div>
                         <div class="message-body">
