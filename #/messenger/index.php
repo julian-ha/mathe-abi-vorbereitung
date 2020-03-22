5a9753d1c28fbb4e3c031284c46ad672888a40b8
Julian Häring <julian@julians-mini.speedport_w_921v_1_46_000>
2020-03-22 11:15:02 +0100

    asdf

---
+++ #/messenger/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+//Database connection
+$pdo = new PDO('mysql:host=188.68.47.203;dbname=k93814_matheAbi', 'k93814_matheAbi', 'Sxt0m25?');
+
+//get all messages
+$sql= "SELECT * FROM nachrichten ORDER BY zeitstempel";
+$statement = $pdo->prepare($sql);
+$statement->execute();
+$messages = $statement->fetchAll();
+
+foreach($messages as $message){
+    echo $message['inhalt'] . "<br>";
+}
+?>
