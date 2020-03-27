5e05c465d1f9367d910b3410be17c2c08371e295
Julian Häring <Julian.Haering@plan-b-gmbh.com>
2020-03-27 10:59:39 +0100

    asdf

--- js/client.js
+++ js/client.js
@@ -29,7 +29,7 @@ function sendMessage(){
     xmlhttp.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
             var message = "Ihre Nachricht wurde erfolgreich versandt";
-            inhalt.innerHTML = "";
+            inhalt.value = "";
             sendNotification(message);   
         }
        
